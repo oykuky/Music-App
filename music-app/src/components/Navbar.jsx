4ad8f8d5631e88d474bfea5d5631bb30d226493e
oykuky <oykuky001@gmail.com>
2024-08-27 17:33:37 +0300

    navbar ıcon updated

--- music-app/src/components/Navbar.jsx
+++ music-app/src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { IoIosHome } from "react-icons/io";
 function Navbar() {
   const [search,setSearch] = useState();
   return (
-    <div className='sticky my-2 flex mx-3 mb-2 justify-center '>
+    <div className='sticky my-2 flex mx-3 mb-2 justify-center items-center '>
         <div className='rounded-full items-center bg-gray-800 m-2 p-3 hover:bg-gray-400'>
-         <IoIosHome className='text-gray-600 text-[35px]' />
+         <IoIosHome className='text-gray-600 text-[25px]' />
         </div>
-        <div className='items-center hover:bg-gray-400 flex w-[600px] rounded-full px-5 bg-gray-800'>
+        <div className='items-center hover:bg-gray-400 flex w-[600px] h-full rounded-full px-5 bg-gray-800'>
           <IoSearch className="text-[25px] text-gray-600 "/>
           <input type="text" name="search" value={search}
            placeholder='What do you want to play ?'
