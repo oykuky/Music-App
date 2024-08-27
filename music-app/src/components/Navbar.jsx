45550f667f9f5283b2613ecee96719dce4dbc9a8
oykuky <oykuky001@gmail.com>
2024-08-27 17:23:05 +0300

    updated

--- music-app/src/components/Navbar.jsx
+++ music-app/src/components/Navbar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoSearch } from "react-icons/io5";
 import { IoIosHome } from "react-icons/io";
 
 function Navbar() {
+  const [search,setSearch] = useState();
   return (
-    <div className='sticky top-0 flex mx-1 mb-2 justify-center'>
-      <div className='flex justify-center'>
-       <div className='w-100 h-100  rounded-full bg-gray-800'>
-       <IoIosHome width={50} height={50} className='' />
-       </div>
-       <IoSearch />
-        <input type="text" name="search" placeholder='What do you want to play' className='rounded-2xl py-2 px-5 bg-gray-800 '>
-        </input>
-      </div>
+    <div className='sticky my-2 flex mx-3 mb-2 justify-center '>
+        <div className='rounded-full items-center bg-gray-800 m-2 p-3 hover:bg-gray-400'>
+         <IoIosHome className='text-gray-600 text-[35px]' />
+        </div>
+        <div className='items-center hover:bg-gray-400 flex w-[600px] rounded-full px-5 bg-gray-800'>
+          <IoSearch className="text-[25px] text-gray-600 "/>
+          <input type="text" name="search" value={search}
+           placeholder='What do you want to play ?'
+           onChange={(e)=>setSearch(e.target.value)}
+           className='outline-none bg-transparent p-3 w-full'></input>
+        </div>
     </div>
   )
 }
