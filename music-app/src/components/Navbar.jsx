9925bb55279343c0c114f8ce44580b90d8ae4bfe
oykuky <oykuky001@gmail.com>
2024-08-28 18:33:45 +0300

    responsive navbar updated

--- music-app/src/components/Navbar.jsx
+++ music-app/src/components/Navbar.jsx
@@ -5,23 +5,23 @@ import Image from 'next/image';
 
 function Navbar() {
   const [search,setSearch] = useState();
-  console.log(search,"SEARCHHHHH")
   return (
-    <div className= 'sticky flex justify-center items-center my-1'>
-      <div className='items-center flex mx-5'>
+    <div className= 'sticky flex justify-between items-center my-1 px-5 md:px-3'>
+      <div className='items-center justify-center flex md:pl-80 lg:pl-96 '>
         <div className='rounded-full items-center bg-gray-800 m-2 p-3 hover:bg-gray-400'>
          <IoIosHome className='text-gray-600 text-[25px]' />
         </div>
-        <div className='items-center hover:bg-gray-400 flex w-[600px] h-full rounded-full px-5 bg-gray-800'>
+        <div className='items-center hover:bg-gray-400 flex h-full rounded-full px-5 bg-gray-800 sm:w-80 lg:w-[450px]'>
           <IoSearch className="text-[25px] text-gray-600 "/>
-          <input type="text" name="search" value={search}
+          <input
+            type="text" name="search" value={search}
             placeholder='What do you want to play ?'
             onChange={(e)=>setSearch(e.target.value)}
             className='outline-none bg-transparent p-3 w-full'>
           </input>
         </div>
       </div>
-      <div className='' >
+      <div className='sm:pl-2'>
         <Image src='/noavatar.png' width={45} height={45} className='rounded-full'/>
       </div>
     </div>
