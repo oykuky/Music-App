556b877d8785a0463f17f3f7c4507100136cbaeb
oykuky <oykuky001@gmail.com>
2024-08-31 00:46:02 +0300

    mongo db utils added

---
+++ src/lib/model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 3,
+      max: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      max: 50,
+    },
+    password: {
+      type: String,
+    },
+    img: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
