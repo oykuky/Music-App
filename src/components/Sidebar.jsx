7ecfd6f5aa1d6605c2ffa60deed4e49341bd052b
oykuky <oykuky001@gmail.com>
2024-08-30 22:07:02 +0300

    bg updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,15 +8,15 @@ import { MdOutlineExplore } from "react-icons/md";
 function Sidebar() {
   return(
    <div className="bg-gray-800 bg-opacity-50 h-full w-1/5 rounded-lg text-white font-semibold gap-3 flex flex-col">
-      <Link href="" className="flex flex-row gap-2 items-center bg-gray-700 hover:bg-opacity-50 rounded-full p-2 m-2">
+      <Link href="" className="flex flex-row gap-2 items-center bg-gray-700 hover:bg-pink-400 rounded-full p-2 m-2">
         <BiLibrary className="h-8 w-8"/>
         <h3>Your Library</h3>
       </Link>
-      <Link href="" className="flex flex-row gap-2 items-center bg-gray-700 hover:bg-opacity-50 rounded-full p-2 m-2">
+      <Link href="" className="flex flex-row gap-2 items-center bg-gray-700 hover:bg-pink-400 rounded-full p-2 m-2">
         <MdFavoriteBorder className="h-8 w-8"/>
         <h3>Favorites</h3>
       </Link>
-      <Link href="" className="flex flex-row gap-2 items-center bg-gray-700 hover:bg-opacity-50 rounded-full p-2 m-2">
+      <Link href="" className="flex flex-row gap-2 items-center bg-gray-700 hover:bg-pink-400 rounded-full p-2 m-2">
         <MdOutlineExplore className="h-8 w-8" />
         <h3>Explore</h3>
       </Link>
