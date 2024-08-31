712bc6812f00009f3ce233706df2c16c1707fca7
oykuky <oykuky001@gmail.com>
2024-08-31 14:20:04 +0300

    RegisterForm ui added

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import React from 'react'
+
+function RegisterForm() {
+  return (
+    <div>
+      <form className='flex flex-col gap-10' onSubmit="">
+            <input type="text" name="username" placeholder="Username" className='rounded-lg h-12 text-center bg-gray-900'></input>
+            <input type="email" name="email" placeholder="Email" className='rounded-lg h-12 text-center bg-gray-900'></input>
+            <input type="password" name="password" placeholder="Password" className='rounded-lg h-12 text-center bg-gray-900'></input>
+            <input type="password" name="passwordRepeat" placeholder="Password Again" className='rounded-lg h-12 text-center bg-gray-900'></input>
+            <button type="button" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-semibold rounded-full text-m px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900">Register</button>
+            <Link className='text-lg text-white' href="/login">
+            {"Have an account?"} <b className='hover:text-pink-600 text-[20px]'>Login</b>
+            </Link>
+        </form>
+    </div>
+  )
+}
+
+export default RegisterForm
