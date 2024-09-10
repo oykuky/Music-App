fd80618fdd5deef480c8758fe018989a7291de3c
oykuky <oykuky001@gmail.com>
2024-09-11 00:31:35 +0300

    sidebar link pages added

---
+++ src/pages/explore/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Explore() {
+  return (
+    <div className="w-full h-full bg-gradient-to-t
+    from-green-900 via-yellow-600 to-pink-400 bg-opacity-50 rounded-lg"> 
+        
+      Explore
+    </div>
+  )
+}
+
+export default Explore
---
+++ src/pages/favorites/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Favorites() {
+  return (
+    <div className="w-full h-full bg-gradient-to-t
+    from-green-900 via-yellow-600 to-pink-400 bg-opacity-50 rounded-lg"> 
+        
+      favorites
+    </div>
+  )
+}
+
+export default Favorites
