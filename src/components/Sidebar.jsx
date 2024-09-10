638329b83d390eb6c7e9e4d7449b12821656faec
oykuky <oykuky001@gmail.com>
2024-09-11 00:32:37 +0300

    Sidebar links updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,35 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import { BiLibrary } from "react-icons/bi";
 import { MdFavoriteBorder } from "react-icons/md";
 import { MdOutlineExplore } from "react-icons/md";
+import { IoIosHome } from "react-icons/io";
+import { useRouter } from "next/router";
 
 
 function Sidebar() {
+  const router = useRouter();
+  const isActive = (pathname) => router.pathname === pathname;
   return(
    <div className="bg-gray-800 bg-opacity-50 h-full w-1/5 rounded-lg text-white font-semibold gap-3 flex flex-col">
-      <Link href="" className="flex flex-row gap-2 items-center bg-gray-700 hover:bg-pink-400 rounded-full p-2 m-2">
+      <Link href="/" 
+         className={`flex flex-row gap-2 items-center ${isActive("/") ? "bg-pink-400" : "bg-gray-700"}
+         hover:bg-pink-400 rounded-full p-2 m-2`}>
+        <IoIosHome className="h-8 w-8"/>
+        <h3>Home</h3>
+      </Link>
+      <Link href="" className={`flex flex-row gap-2 items-center ${isActive("") ? "bg-pink-400" : "bg-gray-700"}
+         hover:bg-pink-400 rounded-full p-2 m-2`}>
         <BiLibrary className="h-8 w-8"/>
         <h3>Your Library</h3>
       </Link>
-      <Link href="" className="flex flex-row gap-2 items-center bg-gray-700 hover:bg-pink-400 rounded-full p-2 m-2">
+      <Link href="/favorites" className={`flex flex-row gap-2 items-center ${isActive("/favorites") ? "bg-pink-400" : "bg-gray-700"}
+         hover:bg-pink-400 rounded-full p-2 m-2`}>
         <MdFavoriteBorder className="h-8 w-8"/>
         <h3>Favorites</h3>
       </Link>
-      <Link href="" className="flex flex-row gap-2 items-center bg-gray-700 hover:bg-pink-400 rounded-full p-2 m-2">
+      <Link href="/explore" className={`flex flex-row gap-2 items-center ${isActive("/explore") ? "bg-pink-400" : "bg-gray-700"}
+         hover:bg-pink-400 rounded-full p-2 m-2`}>
         <MdOutlineExplore className="h-8 w-8" />
         <h3>Explore</h3>
       </Link>
