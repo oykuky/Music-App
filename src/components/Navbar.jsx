c4d457bde41ae0a32ab03edc0b768e15219a899b
oykuky <oykuky001@gmail.com>
2024-09-12 13:17:28 +0300

    navbar layout updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,19 @@ import React, { useState } from 'react'
 import { IoSearch } from "react-icons/io5";
 import { IoIosHome } from "react-icons/io";
 import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 
 
 function Navbar() {
   const [search,setSearch] = useState();
+  const {data:session} = useSession();
+  console.log("SESİİON",session);
   return (
-    <div className= 'sticky h-20 flex justify-between items-center px-4 md:px-3 bg-black bg-opacity-50 my-2'>
+    <div className= 'h-20 flex justify-between items-center px-4 md:px-3 bg-black bg-opacity-50 my-2'>
       <div>
-       <img src='/logo.svg' className='bg-yellow-500 rounded-full w-30 h-30 p-1'></img>
+       <img src='/logo.svg' alt ="logo" className='bg-yellow-500 rounded-full w-30 h-30 p-1'></img>
       </div>
-      <div className='items-center justify-center flex md:pl-80 lg:pl-96 '>
+      <div className='items-center justify-center flex'>
         <div className='rounded-full items-center mr-3 bg-gray-800 p-3 hover:bg-gray-500'>
          <IoIosHome className='text-gray-600 text-[28px]' />
         </div>
@@ -25,9 +28,24 @@ function Navbar() {
           </input>
         </div>
       </div>
-      <div className='border-yellow-600 border-[4px] rounded-full'>
-        <Image src='/noavatar.png' width={45} height={45} className='rounded-full cursor-pointer'/>
-      </div>
+     <div>
+      { session ?
+        (
+          <div className='flex justify-center items-center gap-2'>
+            <div>
+             <button className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-semibold rounded-full text-m px-5 py-2.5 text-center me-2 dark:focus:ring-yellow-900">Log out</button>
+            </div>
+            <div className='border-yellow-600 border-[4px] rounded-full '>
+             <Image src='/noavatar.png' alt ="profileimg" width={40} height={40} className='rounded-full cursor-pointer'/>
+            </div>
+          </div>
+        )
+       : 
+        (<div>
+          <button className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-semibold rounded-full text-m px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900">Login</button>) 
+        </div> )
+      }
+     </div>
     </div>
   )
 }
