05f1c56cc83e0464f7092a02ee179864725247e5
oykuky <oykuky001@gmail.com>
2024-09-13 14:55:54 +0300

    styles updated

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,12 +4,12 @@ import { MdFavoriteBorder } from "react-icons/md";
 
 function SongCard() {
   return (
-    <div className='mx-12'>
-      <div className='flex flex-col justify-center items-center gap-3 mt-3 rounded-xl p-5 bg-black opacity-55 text-gray-800 w-[250px] h-[380px]'>
-        <Image src="/noavatar.png" alt="Songimg" width={250} height={150} className='rounded-xl mt-7'/>
-        <h2 className='text-white'>Title</h2>
-        <div className='bg-pink-300 rounded-full p-1.5'>
-          <MdFavoriteBorder className="h-9 w-9 fill-pink-600"/>
+    <div className='mx-5'>
+      <div className='flex flex-col justify-center items-center gap-3 mt-3 rounded-xl p-5 bg-black opacity-55 text-gray-800 w-[125px] h-[190px] md:w-[230px] md:h-[350px]'>
+        <Image src="/noavatar.png" alt="Songimg" width={200} height={150} className='rounded-xl mt-7'/>
+        <h2 className='text-white font-bold'>Title</h2>
+        <div className='bg-transparent rounded-full cursor-pointer hover:bg-pink-400 p-1.5'>
+          <MdFavoriteBorder className="h-8 w-8 fill-pink-600"/>
         </div>
       </div>
     </div>
