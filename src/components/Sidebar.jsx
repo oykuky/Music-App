79fa17b535b3820f25bfbef99699215c1779bbdb
oykuky <oykuky001@gmail.com>
2024-09-19 16:19:04 +0300

    style updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,24 +11,24 @@ function Sidebar() {
   const router = useRouter();
   const isActive = (pathname) => router.pathname === pathname;
   return(
-   <div className="bg-gray-800 bg-opacity-50 h-full w-1/5 rounded-lg text-white font-semibold gap-2 flex flex-col">
+   <div className="bg-black bg-opacity-50 h-full w-1/5 rounded-lg text-white font-semibold gap-2 flex flex-col">
       <Link href="/" 
-         className={`flex flex-row gap-2 items-center  ${isActive("/") ? "text-pink-400" : "text-white"}
+         className={`flex flex-row gap-2 items-center  ${isActive("/") ? "text-yellow-300" : "text-white"}
          rounded-full p-2 m-2`}>
         <IoIosHome className='h-8 w-8'/>
         <h3>Home</h3>
       </Link>
-      <Link href=""className={`flex flex-row gap-2 items-center  ${isActive("") ? "text-pink-400" : "text-white"}
+      <Link href=""className={`flex flex-row gap-2 items-center  ${isActive("") ? "text-yellow-300" : "text-white"}
         rounded-full p-2 m-2`}>
         <BiLibrary  className='h-8 w-8' />
         <h3>Your Library</h3>
       </Link>
-      <Link href="/favorites"  className={`flex flex-row gap-2 items-center  ${isActive("/favorites") ? "text-pink-400" : "text-white"}
+      <Link href="/favorites"  className={`flex flex-row gap-2 items-center  ${isActive("/favorites") ? "text-yellow-300" : "text-white"}
         rounded-full p-2 m-2`}>
         <MdFavoriteBorder  className='h-8 w-8' />
         <h3>Favorites</h3>
       </Link>
-      <Link href="/explore" className={`flex flex-row gap-2 items-center  ${isActive("/explore") ? "text-pink-400" : "text-white"}
+      <Link href="/explore" className={`flex flex-row gap-2 items-center  ${isActive("/explore") ? "text-yellow-300" : "text-white"}
         rounded-full p-2 m-2`}>
         <MdOutlineExplore  className='h-8 w-8' />
         <h3>Explore</h3>
--- src/pages/favorites/index.jsx
+++ src/pages/favorites/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Favorites() {
   return (
     <div className="w-full h-full bg-gradient-to-t
-    from-green-900 via-yellow-600 to-pink-400 bg-opacity-50 rounded-lg"> 
+     from-black to-purple-600 bg-opacity-50 rounded-lg"> 
         <div className='rounded-b-2xl mx-12 px-4 h-24 flex items-center gap-9 bg-gray-950 opacity-55'>
           <h3 className='text-white font-semibold text-[24px] ml-12'>Favorites</h3>
       </div>
