2efaaae9ca23188032f50643856e62f4f93f8517
oykuky <oykuky001@gmail.com>
2024-10-02 15:07:39 +0300

    sessionProvider added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,16 @@ import AppWrapper from "@/components/AppWrapper";
 import { store } from "@/redux/store";
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
+import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Provider store={store}>
-    <AppWrapper >
-      <Component {...pageProps} />
-    </AppWrapper>
-    </Provider>
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        <AppWrapper>
+          <Component {...pageProps} />
+        </AppWrapper>
+      </Provider>
+    </SessionProvider>
   );
 }
