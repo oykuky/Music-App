45c9f8ce17a42031e21f640dc421b7f2eed3e4ce
oykuky <oykuky001@gmail.com>
2024-10-03 17:55:17 +0300

    librariy page added

--- src/pages/playlistLibrariy/index.jsx
+++ src/pages/playlistLibrariy/index.jsx
@@ -1,38 +1,35 @@
-import { fetchPlaylist } from '@/redux/musicSlice';
+import PlaylistCard from '@/components/PlaylistCard';
+import { fetchFavorites } from '@/redux/musicSlice';
+import { useSession } from 'next-auth/react';
 import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { useDispatch, useSelector } from 'react-redux';
 
+
+//favorites yerine playlist olucak
 function PlaylistLibrariy() {
-    const [playlistId, setPlaylistId] = useState('');
-    const [debouncedPlaylist, setDebouncedPlaylist] = useState('');
-    const dispatch = useDispatch();
-    const { playlist, status, error } = useSelector((state) => state.music);
-    console.log(debouncedPlaylist)
-    console.log("playlistplaylist: " , playlist)
-    useEffect(() => {
-        const handler = setTimeout(() => {
-            setDebouncedPlaylist(playlistId);
-        }, 500);
-      
-        return () => {
-          clearTimeout(handler); // Yeni bir arama başladığında önceki timer temizlenir
-        };
-      }, [playlistId]);
-      
-      // Debounced playlist ismi değiştiğinde fetch işlemi başlar
-      useEffect(() => {
-        if (debouncedPlaylist) {
-          dispatch(fetchPlaylist(debouncedPlaylist));
-        }
-      }, [debouncedPlaylist, dispatch]);
-  
- 
+  const dispatch = useDispatch();
+  const { data: session } = useSession();
+  const favorites = useSelector((state) => state.music.favorites); // Favorileri al
+  useEffect(() => {
+    if (session) {
+      dispatch(fetchFavorites());
+    }
+  }, [session, dispatch]);// Favorileri al
+
   return (
     <div className="w-full h-full bg-gradient-to-t from-black to-purple-600 bg-opacity-50 rounded-lg overflow-y-auto">
-     
+     <div className='rounded-b-xl mx-12 px-4 h-24 flex items-center gap-9 bg-gray-950 opacity-55'>
+          <h3 className='text-white font-semibold text-[24px] ml-12'>Playlist</h3>
+      </div>
 
+        <div className='flex-col flex justify-center items-center gap-2'>
+            {favorites.map((song, i) => (
+              <PlaylistCard key={song.id} song={song} i={i} data={[]} />
+            ))}
+        </div>
+    
     </div>
   );
 }
