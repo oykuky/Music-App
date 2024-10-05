7fe4ae4a76c3ea02afe326a75e2bf0713a6661fa
Emre Mert Akdağ <emremakdag55@gmail.com>
2024-10-06 02:09:05 +0300

    Refactor AppWrapper component and remove Player component

--- src/components/AppWrapper.jsx
+++ src/components/AppWrapper.jsx
@@ -1,29 +1,32 @@
-'use client'
+"use client";
 import { useRouter } from "next/router";
+import { useMemo } from "react";
 import Navbar from "./Navbar";
-import Player from "./Player";
 import Sidebar from "./Sidebar";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import MusicPlayer from "./MusicPlayer";
 
-
 export default function AppWrapper({ children }) {
+  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+
   const router = useRouter();
-  const isAuthPage = ["/login", "/register"].includes(router.pathname);
+  const isAuthPage = useMemo(
+    () => ["/login", "/register"].includes(router.pathname),
+    [router.pathname],
+  );
+
   return (
-    <div className="flex flex-col min-h-screen h-screen bg-black">
-      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
+    <div className="flex h-screen min-h-screen flex-col bg-black">
+      <GoogleOAuthProvider clientId={clientId}>
         {!isAuthPage && <Navbar />}
-        <div className="flex flex-cols h-full space-x-2">
+        <div className="flex-cols flex h-full space-x-2">
           {!isAuthPage && <Sidebar />}
-          <div className="w-full md:h-[calc(100vh-10rem)] h-[calc(100vh-8rem)] rounded-lg">
+          <div className="h-[calc(100vh-8rem)] rounded-lg w-full md:h-[calc(100vh-10rem)]">
             {children}
-          <MusicPlayer/>
+            {!isAuthPage && <MusicPlayer />}
           </div>
         </div>
-        {!isAuthPage && <Player />}
       </GoogleOAuthProvider>
-  </div>
+    </div>
   );
 }
-
--- src/components/Player.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Player() {
-  return <div className="w-full h-28 bg-black">Player</div>;
-}
-
-export default Player;
