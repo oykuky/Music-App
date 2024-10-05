a53f8723e506449150f05a570cb56b1e8ff4cb11
oykuky <oykuky001@gmail.com>
2024-10-05 23:50:25 +0300

    loading state

--- src/pages/playlistLibrariy/index.jsx
+++ src/pages/playlistLibrariy/index.jsx
@@ -1,7 +1,6 @@
 import PlaylistCard from '@/components/PlaylistCard';
 import { fetchPlaylist } from '@/redux/musicSlice';
 import { useSession } from 'next-auth/react';
-import Image from 'next/image';
 import React, { useEffect } from 'react';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { useDispatch, useSelector } from 'react-redux';
@@ -11,12 +10,14 @@ import { useDispatch, useSelector } from 'react-redux';
 function PlaylistLibrariy() {
   const dispatch = useDispatch();
   const { data: session } = useSession();
-  const playlist = useSelector((state) => state.music.playlist); // Favorileri al
+  const {status,error} = useSelector((state) => state.music); 
+  const playlist = useSelector((state) => state.music.playlist); 
+
   useEffect(() => {
     if (session) {
       dispatch(fetchPlaylist());
     }
-  }, [session, dispatch]);// Favorileri al
+  }, [session, dispatch])
 
   return (
     <div className="w-full h-full bg-gradient-to-t from-black to-purple-600 bg-opacity-50 rounded-lg overflow-y-auto">
@@ -24,11 +25,24 @@ function PlaylistLibrariy() {
           <h3 className='text-white font-semibold text-[24px] ml-12'>Playlist</h3>
       </div>
 
-      <div className='flex-col flex justify-center items-center lg:px-16 md:px-12 sm:px-10 px-8'>
+      {status === 'loading' && (
+            <div className="flex justify-center items-center mt-10">
+            <div className="loader">
+             <AiOutlineLoading3Quarters className="animate-spin text-4xl"/>
+            </div>
+          </div>
+          )}
+          {status === 'failed' && <div>Error: {error}</div>}
+          {status === 'succeeded' && ( 
+            <div className='flex-col flex justify-center items-center lg:px-16 md:px-12 sm:px-10 px-8'>
             {playlist.map((song, i) => (
               <PlaylistCard key={song.id} song={song} i={i} data={[]} />
             ))}
-      </div>
+           </div>)
+           
+          }
+
+     
     
     </div>
   );
